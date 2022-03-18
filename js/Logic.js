b07ec43dc39f9b54e750b598280909c30f9cca16
Loic Mas <ciol2006@gmail.com>
2022-03-18 16:18:20 -0700

    Logic added and readdy to be played !!!

--- js/Logic.js
+++ js/Logic.js
@@ -1,18 +1,72 @@
 export default class Logic {
   constructor() {
-    this.turn = "X";
+    this.playerTag = "X";
     this.board = new Array(9).fill(null);
   }
 
   nextTurn() {
-    this.turn = this.turn === "X" ? "O" : "X";
+    this.playerTag = this.playerTag === "X" ? "O" : "X";
   }
 
   selectTile(i) {
+    if (!this.stillPlaying()) {
+      return;
+    }
+
     if (this.board[i]) {
       return;
     }
-    this.board[i] = this.turn;
-    this.nextTurn();
+
+    this.board[i] = this.playerTag;
+
+    if (!this.winningCombination()) {
+      this.nextTurn();
+    }
+  }
+
+  onTileClick(i) {
+    this.selectTile(i);
+    console.log(this.board);
+  }
+
+  winningCombination() {
+    const wayToWin = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const combination of wayToWin) {
+      const [a, b, c] = combination;
+
+      if (
+        this.board[a] &&
+        this.board[a] === this.board[b] &&
+        this.board[b] === this.board[c]
+      ) {
+        return combination;
+      }
+    }
+
+    return null;
+  }
+
+  stillPlaying() {
+    return this.board.includes(null) && !this.winningCombination();
+  }
+
+  hoverText() {
+    tiles.forEach((tile) => {
+      tile.classList.remove("X_hover");
+      tile.classList.remove("O_hover");
+      if (tile.innerText == "") {
+        tile.classList.add(`${this.playerTag}_hover`);
+      }
+    });
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,77 @@
 import Logic from "./Logic.js";
 
+const tiles = document.querySelectorAll(".tile");
 let game = new Logic();
+
+function move() {
+  tiles.forEach((tile) =>
+    tile.addEventListener("click", (i) => {
+      if (game.onTileClick) {
+        if (game.board[tile.dataset.index]) {
+          return;
+        }
+        if (!game.stillPlaying()) {
+          return;
+        }
+        tile.innerText = game.playerTag;
+        game.onTileClick(tile.dataset.index);
+
+        //display who's turn it is.
+        document.querySelector(
+          ".playerTurn"
+        ).textContent = `${game.playerTag}'s turn`;
+
+        //hover text
+        hoverText();
+
+        //winner action
+
+        // Display for the winner.
+        if (game.winningCombination()) {
+          document.querySelector(
+            ".winner"
+          ).textContent = `${game.playerTag} is the winner !!!`;
+          return;
+        }
+        //Display for tie.
+        if (!game.board.includes(null)) {
+          document.querySelector(".winner").textContent = "It's a Tie";
+        }
+      }
+    })
+  );
+}
+
+function hoverText() {
+  tiles.forEach((tile) => {
+    tile.classList.remove("X_hover");
+    tile.classList.remove("O_hover");
+    if (game.winningCombination()) {
+      return;
+    }
+    if (tile.innerText == "") {
+      tile.classList.add(`${game.playerTag}_hover`);
+    }
+  });
+}
+
+function gameStart() {
+  hoverText();
+  move();
+}
+
+function onRestart() {
+  document.querySelector(".restart").addEventListener("click", () => {
+    document.querySelector(".winner").textContent = "";
+    document.querySelectorAll(".tile").textContent = " ";
+    document.querySelector(".playerTurn").textContent = `X's turn`;
+    tiles.forEach((tile) => {
+      tile.textContent = "";
+    });
+    game = new Logic();
+    hoverText();
+  });
+}
+
+gameStart();
+onRestart();
